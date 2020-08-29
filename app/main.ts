4fbcb1913cf98ee6a28c7fe14c936559b871330c
tkt <tktcorporation.go@gmail.com>
2020-08-29 17:00:39 +0900

    it woks fine with some endpoints!

--- app/main.ts
+++ app/main.ts
@@ -7,18 +7,14 @@ const port = Number.parseInt(env.PORT) || 8080;
 
 // const app = new Application();
 
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-
 // console.log(`Listening on port ${PORT} ...`);
 // await app.listen(`${HOST}:${PORT}`);
 
 // const port = 8000;
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello Deno";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.addEventListener("listen", () => {
   console.log(`Listening on localhost:${port}`);
